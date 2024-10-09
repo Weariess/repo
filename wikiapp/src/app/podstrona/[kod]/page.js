c160b662f9e3c9a9f1d24bbc67353736e473af8b
Weariess <125036889+Weariess@users.noreply.github.com>
2024-10-09 11:11:38 +0200

    yup

--- wikiapp/src/app/podstrona/[kod]/page.js
+++ wikiapp/src/app/podstrona/[kod]/page.js
@@ -1,46 +1,42 @@
 "use client"
-import { UNDERSCORE_NOT_FOUND_ROUTE } from 'next/dist/shared/lib/constants'
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
+import Image from "next/legacy/image"
 
 export default function Info({params}){
 
     const[kraj, setKraj] = useState([])
-    const[error, setError] = useState(false)
     const[load, setLoad] = useState(true)
+
     useEffect(()=>{
-        async function getData() {
+        async function getData(){
             try{
                 const response = await fetch(`https://restcountries.com/v3.1/alpha/${params.kod}`)
                 const json = await response.json()
-                console.log(json)
                 setKraj(json)
-            }catch(error){
-               setError(true)
+                console.log(json)
+
+            }catch{
 
             }finally{
                 setLoad(false)
             }
         }
-
         getData()
     }, [])
-
-    useEffect(()=>{
-        if(kraj[0] == undefined){
-            setError(true)
-        }
-        else if(kraj[0]!=undefined){
-            setError(false)
-        }
-    },[kraj])
-            
     
-
     return(
-        <div>
-            {error && <h1>Nie udalo sie pobrac danych</h1>}
+        <div className="flex flex-col items-center justify-center h-screen gap-2">
             {load && <h1>Pobieranie danych</h1>}
-            {kraj.length>0 && <h1>Info o kraju: {kraj[0].name.common}</h1>}
+            {kraj.length>0
+            && <Image 
+            src={kraj[0].flags.png}
+            width={"800px"}
+            height={"400px"}
+            alt={kraj[0].name.common}
+            />}
+            {kraj.length>0 && <h1>{kraj[0].name.common}</h1>}
+            {kraj.length>0 && <h2>{kraj[0].cca2}</h2>}
         </div>
     )
+
 }
