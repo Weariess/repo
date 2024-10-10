22e21d13ad88cb61b6a775f99f4c2f6ba620db98
Weariess <125036889+Weariess@users.noreply.github.com>
2024-10-10 15:52:12 +0200

    hellyeah

--- wikiapp/src/app/podstrona/[kod]/page.js
+++ wikiapp/src/app/podstrona/[kod]/page.js
@@ -1,42 +1,58 @@
 "use client"
-import { useState, useEffect } from 'react'
-import Image from "next/legacy/image"
-
-export default function Info({params}){
-
-    const[kraj, setKraj] = useState([])
-    const[load, setLoad] = useState(true)
+import { useState, useEffect} from "react"
+import Image from "next/image"
+import Borders from "@/components/borders"
+export default function KrajInfo({params}){
+    
+const [kraj, setKraj] = useState({})
+const[pobieranie, setPobieranie] = useState(true)
+const [error, setError] = useState(false)
 
     useEffect(()=>{
-        async function getData(){
+        const getData = async ()=>{
+
             try{
                 const response = await fetch(`https://restcountries.com/v3.1/alpha/${params.kod}`)
                 const json = await response.json()
-                setKraj(json)
                 console.log(json)
+                setKraj(json[0])
 
-            }catch{
-
+                if(response.status == 400){
+                    setError(true)
+                }
+    
+            }catch(error){
+                setError(true)
+                console.log(error)
             }finally{
-                setLoad(false)
+                setPobieranie(false)
             }
         }
         getData()
-    }, [])
-    
+    },[])
+
+
     return(
-        <div className="flex flex-col items-center justify-center h-screen gap-2">
-            {load && <h1>Pobieranie danych</h1>}
-            {kraj.length>0
-            && <Image 
-            src={kraj[0].flags.png}
-            width={"800px"}
-            height={"400px"}
-            alt={kraj[0].name.common}
-            />}
-            {kraj.length>0 && <h1>{kraj[0].name.common}</h1>}
-            {kraj.length>0 && <h2>{kraj[0].cca2}</h2>}
+        <div>
+            {pobieranie && <h1>Pobieranie danych...</h1>}
+            {error && <h1>Nie udało się pobrać danych</h1>}
+            {kraj.cca2 != null && 
+            <div><div class="flex flex-col justify-center items-center text-center min-screen">
+            <Image src={kraj.flags.png} width={600} height={200}></Image>
+            <h1>{(kraj.name.common).toUpperCase()}({(kraj.cca2).toLowerCase()})</h1>
+            <h1>capital: {kraj.capital[0]}</h1>
+            <h1>population: {kraj.population}</h1>
+            </div>
+
+            <div className="flex flex-wrap gap-2">
+                <Borders borders={kraj.borders}> 
+
+                </Borders>
+            </div>
+            </div>
+        
+
+            }
         </div>
     )
-
 }
--- wikiapp/src/components/menu.js
+++ wikiapp/src/components/menu.js
@@ -3,14 +3,7 @@ import Link from "next/link";
 export default function Menu(){
     return(
         <div className="flex flex-col gap-2">
-            <Link href="/">main</Link>
-            <Link href="/strona1">strona1</Link>
-            <Link href="/strona2">strona2</Link>
-            <Link href="/strona3">strona3</Link>
-            <Link href="/strona4">strona4</Link>
-            <Link href="/strona5">strona5</Link>
-            <Link href="/strona6">strona6</Link>
-            <Link href="/strona7">strona7</Link>
+            <Link href="/glowna">main</Link>
         </div>
         
     )
